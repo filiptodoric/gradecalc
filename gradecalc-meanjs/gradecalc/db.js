35aed9cffc6b866ac4a485277e4c8483df67d714
Filip Todoric <filiptodoric@gmail.com>
2015-10-28 12:53:31 -0400

    Mongoose is not working properly, the code is broken

--- gradecalc-meanjs/gradecalc/db.js
+++ gradecalc-meanjs/gradecalc/db.js
@@ -8,26 +8,35 @@ mongoose.connect('mongodb://localhost/gradecalc');
  * Lets define our Model for User entity. This model represents a collection in the database.
  * We define the possible schema of User document and data types of each field.
  * */
-var User = mongoose.model('User', {email: String, classes: Array});
-
+var Class = mongoose.model('Class', {className: String, marks: [{type: Number}], grades: [{type: Number}]});
+var User = mongoose.model('User', {email: String, classes: [Class] });
 /**
  * Lets Use our Models
  * */
 
 //Lets create a new user
-var user1 = new User({email: 'testemail@some.com', classes: ['COMP1000', 'WYMN1000']});
+var class1 = new Class({className: 'TEST1234', marks: [72, 88, 63], grades: [30, 40, 30]});
+var user1 = new User({email: 'stack@overflow.com', classes: [class1]});
 
 //Some modifications in user object
-user1.name = user1.email.toUpperCase();
+//user1.name = user1.email.toUpperCase();
 
 //Lets try to print and see it. You will see _id is assigned.
-console.log("Bang: " + user1);
+console.log("Bang: " + user1.classes.className);
 
 //Lets save it
+class1.save(function (err, classObj) {
+  if (err) {
+    console.log("CLASS This is the error: " + err);
+  } else {
+    console.log('CLASS saved successfully:', classObj);
+  }
+});
+
 user1.save(function (err, userObj) {
   if (err) {
-    console.log(err);
+    console.log("USER This is the error: " + err);
   } else {
-    console.log('saved successfully:', userObj);
+    console.log('USER saved successfully:', userObj);
   }
 });
