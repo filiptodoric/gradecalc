ed05e7310311be6a7c1cf3677968a77ae69e44a8
Filip Todoric <filiptodoric@gmail.com>
2015-10-29 22:04:01 -0400

    minor change, probable rollback

--- gradecalc-meanjs/gradecalc/db.js
+++ gradecalc-meanjs/gradecalc/db.js
@@ -8,35 +8,29 @@ mongoose.connect('mongodb://localhost/gradecalc');
  * Lets define our Model for User entity. This model represents a collection in the database.
  * We define the possible schema of User document and data types of each field.
  * */
-var Class = mongoose.model('Class', {className: String, marks: [{type: Number}], grades: [{type: Number}]});
-var User = mongoose.model('User', {email: String, classes: [Class] });
+//var Class = mongoose.model('Class', {className: String, marks: [{type: Number}], grades: [{type: Number}]});
+//var User = mongoose.model('User', {email: String, classes: [Class] });
 /**
  * Lets Use our Models
  * */
 
-//Lets create a new user
-var class1 = new Class({className: 'TEST1234', marks: [72, 88, 63], grades: [30, 40, 30]});
-var user1 = new User({email: 'stack@overflow.com', classes: [class1]});
+var Schema = mongoose.Schema;
+var Class = mongoose.model('Class', {className: String, marks: [{type: Number}], grades: [{type: Number}]});
+var User = mongoose.model('User',{
+   email: String,
+   classes: [ {type: Schema.Types.ObjectID, ref: 'Class'}]
+ });
 
-//Some modifications in user object
-//user1.name = user1.email.toUpperCase();
+ var Class1 = new Class({className: 'STAC1234', marks: [72, 88, 63], grades: [30, 40, 30]});
 
-//Lets try to print and see it. You will see _id is assigned.
-console.log("Bang: " + user1.classes.className);
-
-//Lets save it
-class1.save(function (err, classObj) {
-  if (err) {
-    console.log("CLASS This is the error: " + err);
-  } else {
-    console.log('CLASS saved successfully:', classObj);
-  }
-});
-
-user1.save(function (err, userObj) {
-  if (err) {
-    console.log("USER This is the error: " + err);
-  } else {
-    console.log('USER saved successfully:', userObj);
-  }
-});
+ Class1.save(function(err, classData) {
+    var User1 = new User({email: 'me@overflow.com', classes: [class1]})
+    User1.classes.push(classData._id);
+    User1.save(function(err, userData) {
+      if (err) {
+        console.log("CLASS This is the error: " + err);
+      } else {
+        console.log('CLASS saved successfully:', userData);
+      }
+    })
+ });
