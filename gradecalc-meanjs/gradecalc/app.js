c81bbe06ac01b895e94685b05163661f9b9fcb34
Filip Todoric <filiptodoric@gmail.com>
2015-11-15 21:03:12 -0500

    Finally getting stuff back from the database

--- gradecalc-meanjs/gradecalc/app.js
+++ gradecalc-meanjs/gradecalc/app.js
@@ -9,7 +9,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 //var db = require('./db');
 var db = require('./routes/dbCalls');
-
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 var app = express();
 
@@ -29,6 +30,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/', db);
+
+
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
--- gradecalc-meanjs/gradecalc/routes/dbCalls.js
+++ gradecalc-meanjs/gradecalc/routes/dbCalls.js
@@ -2,18 +2,20 @@ var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var url = 'mongodb://localhost:27017/gradecalc';
 var ObjectID = mongodb.ObjectID;
+var express = require('express');
+var router = express.Router();
+// mongodb://localhost/gradecalc
 
 var collection;
 
 var connectToDBs = function(callback) {
-  MongoClient.connect('mongodb://localhost/persistent-notes', function(err, db) {
+  MongoClient.connect(url, function(err, db) {
     if (err) {
       console.log('Unable to connect to the mongoDB server. Error:', err);
       throw err;
     }
     console.log('Connection established to', url);
-
-    var collection = db.collection('users');
+    collection = db.collection('users');
 
     if (callback) {
       callback();
@@ -23,3 +25,28 @@ var connectToDBs = function(callback) {
 
 // connect to the database
 connectToDBs();
+
+router.get('/getFilip', function(req, res) {
+  //console.log(collection.find());
+    var renderUser = function(err, userInfo) {
+        if (err) {
+            userInfo = [{"email": email,
+                      "classes": 'there was an error styll.'}];
+        }
+        res.send(userInfo);
+    }
+
+    if (1 < 2) {
+      //  collection.find({email: 'filiptodoric@gmail.com'}).toArray(renderUser);
+      collection.find({email: 'stodoric@gmail.com'}).toArray(renderUser);
+    } else {
+        res.send([{"email": email,
+                  "classes": 'there was an error styll.'}]);
+    }
+
+
+});
+
+
+
+module.exports = router;
