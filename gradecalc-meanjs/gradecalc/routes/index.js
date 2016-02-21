196a2e5999270db1841bf1a3c2eaf18acb05fe43
Filip Todoric <filiptodoric@gmail.com>
2016-02-21 18:30:51 -0500

    please work

--- gradecalc-meanjs/gradecalc/routes/index.js
+++ gradecalc-meanjs/gradecalc/routes/index.js
@@ -3,16 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("This is the session: " + req.session.email);
 
-  if (req.session.email) {
     res.render('index');
-    console.log("I'm here");
-    getUser(req, res);
-  }
-  else {
-    res.render('index');
-  }
+
 });
 
 module.exports = router;
